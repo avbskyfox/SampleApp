83e417b5d8707523aa18d425c2445c999e7b0464
Alex Bubnov <avb_it@outlook.com>
2016-11-08 23:31:17 +0500

    Make routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
 
-  get 'static_pages/help'
+  match '/help', to: 'static_pages#help', via: 'get'
 
-  get 'static_pages/about'
+  get '/about', to: 'static_pages#help'
+
+  get '/contacts', to: 'static_pages#contacts'
+
+  root to: "static_pages#home"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
